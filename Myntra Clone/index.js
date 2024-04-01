209c444637e75df4635674df5e256d07bc3b07bb
Naman Bansal <www.naman43@gmail.com>
2024-04-01 12:58:58 +0530

    fc neww

---
+++ Myntra Clone/index.js
@@ -0,0 +1,14 @@
+// Crypto Curerency Application
+// Concept: JS ES6, react js, financial apis
+// flip card css
+//https://github.com/naimjeem/React-CryptoCompare
+//
+// Myntra clone 
+// features: no multiple pages: one page with parallax effect(acc to category) women men kids, add glassmorphism
+// men - apparels: ethnic and western, foot wear: ethnic and formals
+// women - apparels: ethnic and western, foot wear: ethnic and formals
+// kids - apparels:summerwaer winterwear, footwear : shoes sneakers flipflop
+// women - accesories: fashion jewellery, earrings
+// synopsis:problem statement, requirements, introduction
+// documentation : Structure of project etc
+// Use Tailwind, Host on Postman, Backend using node on local host
